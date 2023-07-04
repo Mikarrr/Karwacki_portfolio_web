5dc26c57a38c2c3e6ac599f8ba08827d006a8218
Mikołaj <karwacki.mikolaj123@gmail.com>
2023-07-04 17:47:36 +0200

    edit github linnk

--- data/projects.js
+++ data/projects.js
@@ -9,6 +9,6 @@ const projects = [
     name: "PORTFOLIO",
     date: "June 30.2023",
     image: "../photos/portfolio/portfolio.jpg",
-    src: "https://github.com/Mikarrr/futuristic_shop.git",
+    src: "https://github.com/Mikarrr/karwacki_portfolio_web",
   },
 ];
