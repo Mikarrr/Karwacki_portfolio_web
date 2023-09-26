7941e3b93ccb339d825664a49f767ae62f155147
Mikołaj <karwacki.mikolaj123@gmail.com>
2023-09-26 15:50:20 +0200

    edit adddcontent script

--- data/projects.js
+++ data/projects.js
@@ -3,18 +3,21 @@ const projects = [
     name: "THEATRE",
     date: "June 30.2023",
     image: "../photos/js-anim-web/js-anim-web.jpg",
+    image_mobile: "../photos/js-anim-web/js-anim-web-mobile.jpg",
     src: "https://github.com/Mikarrr/DOM-JS-WEBISTE",
   },
   {
     name: "PORTFOLIO",
     date: "June 30.2023",
     image: "../photos/portfolio/portfolio.jpg",
+    image_mobile: "../photos/js-anim-web/js-anim-web-mobile.jpg",
     src: "https://github.com/Mikarrr/karwacki_portfolio_web",
   },
   {
     name: "FUTURISTIC",
     date: "May 15.2023",
     image: "../photos/futuristic/homepage.jpg",
+    image_mobile: "../photos/js-anim-web/js-anim-web-mobile.jpg",
     src: "https://github.com/Mikarrr/futuristic_shop.git",
   },
 ];
--- scripts/addContent.js
+++ scripts/addContent.js
@@ -1,22 +1,29 @@
 let projectsHTML = "";
 
 projects.forEach((project) => {
+  // Ustal, czy rozdzielczość jest poniżej 700px
+  const isMobileResolution = window.innerWidth < 700;
+
+  // Wybierz odpowiednią ścieżkę do obrazka w zależności od rozdzielczości
+  const imageSource = isMobileResolution ? project.image_mobile : project.image;
+
   projectsHTML += `
   <div class="project">
     <h3>${project.name}</h3>
-    <h2>{ ${project.name} }</h2>
+    <h2>${project.name}</h2>
     <img src="../photos/logo.png" alt="" />
     <p>Created - ${project.date}</p>
     <img
       class="project_img"
-      src="${project.image}"
+      src="${imageSource}"
       alt=""
     />
     <div class="image_description">
-    <p>See the highlights of this website.</p>
-    <a href="${project.src}"> <i class="fa fa-github" style="font-size:17px"></i> SOURCE CODE &dharr;</a>
+      <p>See the highlights of this website.</p>
+      <a href="${project.src}"> <i class="fa fa-github" style="font-size:17px"></i> SOURCE CODE &dharr;</a>
     </div>
-    </div>
-    `;
+  </div>
+  `;
 });
+
 document.querySelector(".js-projects-grid").innerHTML = projectsHTML;
