751ecd1bb4258fc0ebd1690c01ce70e59bf684f1
Mikołaj <karwacki.mikolaj123@gmail.com>
2023-09-27 17:47:45 +0200

    add new photo mobile view

--- data/projects.js
+++ data/projects.js
@@ -10,14 +10,14 @@ const projects = [
     name: "PORTFOLIO",
     date: "June 30.2023",
     image: "../photos/portfolio/portfolio.jpg",
-    image_mobile: "../photos/js-anim-web/js-anim-web-mobile.jpg",
+    image_mobile: "../photos/portfolio/portfolio_mobile.jpg",
     src: "https://github.com/Mikarrr/karwacki_portfolio_web",
   },
   {
     name: "FUTURISTIC",
     date: "May 15.2023",
     image: "../photos/futuristic/homepage.jpg",
-    image_mobile: "../photos/js-anim-web/js-anim-web-mobile.jpg",
+    image_mobile: "../photos/futuristic/homepage_mobile.jpg",
     src: "https://github.com/Mikarrr/futuristic_shop.git",
   },
 ];
--- scripts/loader.js
+++ scripts/loader.js
@@ -1,6 +1,18 @@
+// Oczekaj na załadowanie strony
+window.addEventListener("load", () => {
+  // Pobierz element preloadera
+  const preloader = document.querySelector(".preloader");
+
+  // Opcjonalnie, możesz opóźnić animację na kilka sekund
+  setTimeout(() => {
+    // Dodaj klasę z animacją po opóźnieniu
+    preloader.classList.add("animate-preloader");
+  }, 1000); // 2000 milisekund (2 sekundy) opóźnienia
+});
+
 var loader = document.getElementById("preloader");
 window.addEventListener("load", function () {
   setTimeout(() => {
     loader.style.display = "none";
-  }, 1100);
+  }, 2000);
 });
