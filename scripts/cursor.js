4c63d07c0c8b916e2764d3a6e1f3d8e256f92ba9
Mikołaj <karwacki.mikolaj123@gmail.com>
2023-11-27 20:40:25 +0100

    add new cursor

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,11 +1,26 @@
 let mouseCursor = document.querySelector(".cursor");
+let mouseCursorDot = document.querySelector(".dot_cursor");
 let transText = document.querySelectorAll(".transText .anim, .transText h1");
 let changeIndex = document.querySelectorAll(".changeIndex");
+let allhref = document.querySelectorAll("body a");
 window.addEventListener("mousemove", cursor);
 
 function cursor(e) {
-  mouseCursor.style.top = e.pageY + "px";
-  mouseCursor.style.left = e.pageX + "px";
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  mouseCursor.style.left = `${posX}px`;
+  mouseCursor.style.top = `${posY}px`;
+  mouseCursorDot.style.left = `${posX}px`;
+  mouseCursorDot.style.top = `${posY}px`;
+
+  mouseCursor.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    { duration: 200, fill: "forwards" }
+  );
 }
 
 transText.forEach((link) => {
@@ -19,13 +34,24 @@ transText.forEach((link) => {
   });
 });
 
+allhref.forEach((link) => {
+  link.addEventListener("mouseleave", () => {
+    mouseCursorDot.classList.remove("scaleTextHref");
+  });
+  link.addEventListener("mouseover", () => {
+    mouseCursorDot.classList.add("scaleTextHref");
+  });
+});
+
 changeIndex.forEach((link) => {
   link.addEventListener("mouseleave", () => {
     link.classList.remove("main_header_index");
     mouseCursor.classList.remove("cursor_index");
+    mouseCursorDot.classList.remove("cursor_index");
   });
   link.addEventListener("mouseover", () => {
     link.classList.add("main_header_index");
     mouseCursor.classList.add("cursor_index");
+    mouseCursorDot.classList.add("cursor_index");
   });
 });
